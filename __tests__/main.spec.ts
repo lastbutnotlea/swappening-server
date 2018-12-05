fc1480eac2ba882a2ac78241961c4d3826398087
Martin <12Martin21@web.de>
2018-12-05 12:09:07 +0100

    started using the orm sequelize

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,10 +1,40 @@
-import { Delays, greeter } from '../src/main';
+import { Delays, greeter } from "../src/main";
 
-describe('greeter function', () => {
+console.log("Start");
+const Sequelize = require("sequelize");
+const sequelize  = new Sequelize("postgres", "postgres", "password", {
+  host: "vmkemper14.informatik.tu-muenchen.de",
+  dialect: "postgres",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+console.log("Check");
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+console.log("End");
+
+describe("greeter function", () => {
   // Read more about fake timers: http://facebook.github.io/jest/docs/en/timer-mocks.html#content
   jest.useFakeTimers();
 
-  const name: string = 'John';
+  const name: string = "John";
 
   let hello: string;
 
@@ -16,7 +46,7 @@ describe('greeter function', () => {
   });
 
   // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
+  it("delays the greeting by 2 seconds", () => {
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(((setTimeout as Function) as jest.Mock).mock.calls[0][1]).toBe(
       Delays.Long,
@@ -24,7 +54,7 @@ describe('greeter function', () => {
   });
 
   // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
+  it("greets a user with `Hello, {name}` message", () => {
     expect(hello).toBe(`Hello, ${name}`);
   });
 });
