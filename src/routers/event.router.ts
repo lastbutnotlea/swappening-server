31b7a25a0f640b877da3424718069d051e4e6234
AlexBiederer <ga32jum@mytum.de>
2019-03-04 11:51:18 +0100

    Added testing route

--- src/routers/event.router.ts
+++ src/routers/event.router.ts
@@ -261,10 +261,10 @@ eventRouter.post("/image", upload.single("data"), (req, res) => {
 /**
  *  This is a endpoint for uploading form-data which contains the image and the eventId for the image
  */
-eventRouter.post("/image/:storageName/:originalName", (req, res) => {
+eventRouter.post("/image/:eventId/:storageName/:originalName", (req, res) => {
   // TODO Verify these Parameters
   const payload: PictureAddModel = {
-    eventId: req.body.eventId,
+    eventId: req.params.eventId,
     originalName: req.params.originalName,
     pictureStorageName: req.params.storageName,
   };
